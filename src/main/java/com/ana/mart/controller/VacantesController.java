5548790dea1bc3a7a4825cebab09a114a8b65ea0
hp <hp@PC-PedroelMono>
2021-08-02 23:55:43 -0500

    tercercommit

--- src/main/java/com/ana/mart/controller/VacantesController.java
+++ src/main/java/com/ana/mart/controller/VacantesController.java
@@ -93,7 +93,7 @@ public class VacantesController {
 		vacantesService.guardar(vacante);
 		atributo.addFlashAttribute("msg", "Vacante guardada con éxito");
 		//mostrar la vista
-		return "vacantes/indexPaginate";
+		return "vacantes/listaVacantes";
 	}
 	
 	@GetMapping("/crear")
